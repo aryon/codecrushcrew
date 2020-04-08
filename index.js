9c21173b7d7b445baa87fa87be601901368a0568
Brandon Trebitowski <brandon@pixegon.com>
2020-04-08 13:32:55 -0600

    longer sessions

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ mongoose.connect(process.env.MONGO_DB_URL, {
 // Configure cooke session for express
 app.use(express.static('public'));
 app.use(cookieSession({
-  maxAge: 24 * 60 * 60 * 1000 * 30, // One day in milliseconds
+  maxAge: 24 * 60 * 60 * 1000 * 1000, // One day in milliseconds
   keys: ['yum cookies are good']
 }));
 // Used to initialize passport
